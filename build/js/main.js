78a7d6827904b8e8c7b37b01bd55250c99d9a9d9
Tushar Biswas <tusarbiswas888@gmail.com>
2022-08-31 01:25:53 +0600

    Packages section design complete

---
+++ build/js/main.js
@@ -0,0 +1,11 @@
+const leftNav = document.getElementById('left-slide');
+const rightNav = document.getElementById('right-slide');
+const slider = document.getElementById('slider');
+
+leftNav.addEventListener('click', function () {
+    slider.classList.remove("nav-end");
+})
+
+rightNav.addEventListener('click', function () {
+    slider.classList.add("nav-end");
+})
